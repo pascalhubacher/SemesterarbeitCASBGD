c21c4fb7712dba4600555038c0f4ea8770356a27
Pascal Hubacher <pascal.hubacher@outlook.com>
2020-07-19 16:58:29 +0200

    docker-compose first start

--- simulator/simulator.py
+++ simulator/simulator.py
@@ -20,6 +20,44 @@ STR_OTHER = 'other'
 #time laps
 INT_TIME_LAPS = 1
 
+def send_to_kafkaproducer(ip, port, message, topic)
+    #############################
+    # confluent kafka Producer
+    #############################
+    from confluent_kafka import Producer
+
+    p = Producer({'bootstrap.servers': 'localhost:9092'})
+    messages = ["message1","message2","message3"]
+
+    def delivery_report(err, msg):
+        """ Called once for each message produced to indicate delivery result.
+            Triggered by poll() or flush(). """
+        if err is not None:
+            print('Message delivery failed: {}'.format(err))
+        else:
+            print('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
+
+    for data in messages:
+        # Trigger any available delivery report callbacks from previous produce() calls
+        p.poll(0)
+
+        # Asynchronously produce a message, the delivery report callback
+        # will be triggered from poll() above, or flush() below, when the message has
+        # been successfully delivered or failed permanently.
+        
+        # No Key
+        p.produce('test-topic', data.encode('utf-8'), callback=delivery_report)
+        
+        # Key = '1'
+        p.produce('test-topic'
+                , key="1"
+                , value = data.encode('utf-8')
+                , callback=delivery_report)
+
+    # Wait for any outstanding messages to be delivered and delivery report
+    # callbacks to be triggered.
+    p.flush()
+
 def execute_log_data(data_log):
     print(' Process player id {} team ({}) started'.format(data_log[0], data_log[1]))
     with open(data_log[2]) as f:
