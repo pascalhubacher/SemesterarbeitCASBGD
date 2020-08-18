f52060ea1728bdf891c8a8063383417c414a0249
Pascal Hubacher <pascal.hubacher@outlook.com>
2020-08-18 09:15:38 +0200

    faust integrated, schema registry ui

--- faust/wait_for_services.sh
+++ faust/wait_for_services.sh
@@ -4,14 +4,14 @@ set -e
 cmd="$@"
 
 until nc -vz ${KAFKA_BOOSTRAP_SERVER_NAME} ${KAFKA_BOOSTRAP_SERVER_PORT}; do
-  >&2 echo "Waiting for Kafka to be ready... - sleeping"
+  >&2 echo "Waiting for Kafka (${KAFKA_BOOSTRAP_SERVER_NAME} ${KAFKA_BOOSTRAP_SERVER_PORT}) to be ready... - sleeping"
   sleep 2
 done
 
 >&2 echo "Kafka is up - executing command"
 
 until nc -vz ${SCHEMA_REGISTRY_SERVER} ${SCHEMA_REGISTRY_SERVER_PORT}; do
-  >&2 echo "Waiting for Schema Registry to be ready... - sleeping"
+  >&2 echo "Waiting for Schema Registry (${SCHEMA_REGISTRY_SERVER} ${SCHEMA_REGISTRY_SERVER_PORT}) to be ready... - sleeping"
   sleep 2
 done
 
