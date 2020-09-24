9add1a88fccef4a416e95d6f4b3762cb20571161
BeatSuter <67411701+BeatSuter@users.noreply.github.com>
2020-09-24 14:24:46 +0200

    .NET Producer/Consumer

---
+++ .NET/Streamteam-Client/Consumer/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading;
+using Confluent.Kafka;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //const string topicName = "fbEvents";
+            string topicName = "rawGames";
+            //Topic name aus Argumentsliste übernehmen
+            if(args.Length > 0)
+            {
+                topicName = args[0];
+            }
+            Console.WriteLine("Listen to Kafka topic: " + topicName);
+
+            //foreach (string arg in args)
+            //{
+            //    Console.WriteLine("Listen to Kafka topic: " + arg);
+            //}
+
+            var cConfig = new ConsumerConfig
+            {
+                //BootstrapServers = "kafka-1:9092",
+                BootstrapServers = "localhost:9092",
+                //BootstrapServers = "pkc-lq8gm.westeurope.azure.confluent.cloud:9092",
+                //SaslMechanism = SaslMechanism.Plain,
+                //SecurityProtocol = SecurityProtocol.SaslSsl,
+                //SaslUsername = "HD2RDLULRTRHU7Z7",
+                //SaslPassword = "L8bTbJHEmeifXzEBzeFanqM7n88lxHvlqq/xctMaVGaZSlagXtdmRDKO/dESQ3oJ",
+                GroupId = Guid.NewGuid().ToString()
+                //AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+
+            using (var consumer = new ConsumerBuilder<string, string>(cConfig).Build())
+            {
+                consumer.Subscribe(topicName);
+                while (true)
+                {
+                    try
+                    {
+                        var consumeResult = consumer.Consume();
+                        Console.WriteLine($"consumed Key: {consumeResult.Key}; Value {consumeResult.Value}");
+                    }
+                    catch (ConsumeException e)
+                    {
+                        Console.WriteLine($"consume error: {e.Error.Reason}");
+                    }
+
+                    //Thread.Sleep(1000);
+                    //Thread.Sleep(30);
+                }
+
+                consumer.Close();
+            }
+        }
+    }
+}
---
+++ .NET/Streamteam-Client/Producer/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+
+namespace Producer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //const string topicName = "fbEvents";
+            string topicName = "demoBizNet";
+            //Topic name aus Argumentsliste übernehmen
+            if (args.Length > 0)
+            {
+                topicName = args[0];
+            }
+            Console.WriteLine("Listen to Kafka topic: " + topicName);
+
+
+            var pConfig = new ProducerConfig
+            {
+                //BootstrapServers = "kafka-1:9092",
+                BootstrapServers = "localhost:9092",
+                //SaslMechanism = SaslMechanism.Plain,
+                //SecurityProtocol = SecurityProtocol.SaslSsl,
+                // Note: If your root CA certificates are in an unusual location you
+                // may need to specify this using the SslCaLocation property.
+                //SaslUsername = "HD2RDLULRTRHU7Z7",
+                //SaslPassword = "L8bTbJHEmeifXzEBzeFanqM7n88lxHvlqq/xctMaVGaZSlagXtdmRDKO/dESQ3oJ"
+            };
+
+            using (var producer = new ProducerBuilder<Null, string>(pConfig).Build())
+            {
+                var idx = 0;
+                while (true)
+                {
+                    var val = $"test {DateTime.Now: yyyy.MM.dd-HH:mm:ss}";
+                    var result = await producer.ProduceAsync(topicName, new Message<Null, string> {Value = val })
+                        .ContinueWith(task => task.IsFaulted
+                            ? $"error producing message: {task.Exception.Message}"
+                            : $"produced to: {task.Result.TopicPartitionOffset}: {val}");
+
+                    Console.WriteLine(result);
+                    //// block until all in-flight produce requests have completed (successfully
+                    //// or otherwise) or 10s has elapsed.
+                    //producer.Flush(TimeSpan.FromSeconds(10));
+                    Thread.Sleep(2000);
+                    idx++;
+                    if (idx > 10)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
